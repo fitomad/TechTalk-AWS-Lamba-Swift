489332dea35db3a5b899c102d65a570da13bb84d
Adolfo <adolfo.vera@outlook.com>
2023-03-16 09:56:16 +0100

    Fixed SRP at Dashboard use case and repository.

--- App/GlobantPlus/Domain/Repositories/DashboardRepository.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-protocol DashboardRepository {
-    func fetchTrendingMovies() async throws -> [TrendingItem]
-    func fetchTrendingShows() async throws -> [TrendingItem]
-    func fetchPopularDocumentaries() async throws -> [TrendingItem]
-}
